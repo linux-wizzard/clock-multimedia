606656de5751385fc37268f6006d4c38061a2b45
XXX <ballak@seznam.cz>
2018-01-13 21:23:29 +0100

    edit install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir ~/.conky/clock-multimedia
 chmod +x scripts/track-info.sh scripts/vlc-info.sh
+chmod +x uninstall.sh
 cp clock-widget ~/.conky/clock-multimedia
 cp -rf icons ~/.conky/clock-multimedia
 cp -rf scripts ~/.conky/clock-multimedia
